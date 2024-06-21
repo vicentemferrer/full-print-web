c7bbe26fafd2b394e3e92a51f896674aa2dfb09b
Vicente <vicenteferrer_01@hotmail.com>
2024-06-21 10:25:48 -0400

    Adding React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vicentemferrer.github.io/',
-  base: 'full-print-web'
+  base: 'full-print-web',
+  integrations: [react()]
 });
